9e1a737934549f8001a0af49a81f07d79bfe9bd8
mtyBelle <mokty2004@gmail.com>
2026-01-09 15:15:23 +0000

    alarm with vital classes to test

--- RPM_monitor/src/main/java/Main.java
+++ RPM_monitor/src/main/java/Main.java
@@ -1,7 +1,15 @@
+import Alarm.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        VitalSign hr = new HeartRate("P001", 130);
+        Alarm alarm = new Alarm(hr);
+
+        if (alarm.getAlarmLevel() != AlarmLevel.GREEN) {
+            alarm.sendNotification();
+        }
     }
 
 }
