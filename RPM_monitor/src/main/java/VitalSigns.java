dbb2ac2b050b6deadbcaaa9f0ea0657e467efe0f
chel0614 <86668561+chel0614@users.noreply.github.com>
2026-01-10 22:04:23 +0800

    Rachel's changing it again

--- RPM_monitor/src/main/java/VitalSigns.java
+++
@@ -1,7 +0,0 @@
-public enum VitalSigns {
-    Temperature,
-    HeartRate,
-    RespiratoryRate,
-    BloodPressure,
-    ECG
-}
--- RPM_monitor/src/main/java/rpm/domain/domain.java
+++ RPM_monitor/src/main/java/rpm/domain/domain.java
@@ -1,4 +1,4 @@
-package rdm.domain;
+package rpm.domain;
 
 //•	VitalType（TEMP/HEART_RATE/RESP_RATE/SYSTOLIC_BP/DIASTOLIC_BP/ECG）
 //      •	VitalSample（patientId, ts, type, value, unit）
